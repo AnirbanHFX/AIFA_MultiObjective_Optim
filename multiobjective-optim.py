97b37598c43374ee8fdad833465c8aa70eb2fe54
AnirbanHFX <iamanirbanmajumder@gmail.com>
2022-03-03 22:15:18 +0530

    Added IDMOA to multiobjective-optim (works)

--- multiobjective-optim.py
+++ multiobjective-optim.py
@@ -1,6 +1,5 @@
 import csv
 import copy
-from lib2to3.pgen2 import grammar
 
 class Graph:
 
@@ -55,6 +54,20 @@ class Graph:
             'next' : vertex_B,
             'cost' : cost_vector
         })
+
+    def reset_costs(self):
+        """
+        Reset costs and paths accrued over the course of some prior search algorithm
+        """
+        for vertex in self.vertex_list:
+            assert isinstance(self.vertex_list[vertex], dict)
+            assert 'G' in self.vertex_list[vertex]
+            assert 'F' in self.vertex_list[vertex]
+            assert 'H' in self.vertex_list[vertex]
+            assert 'path' in self.vertex_list[vertex]
+            self.vertex_list[vertex]['G'] = []
+            self.vertex_list[vertex]['F'] = []
+            self.vertex_list[vertex]['path'] = []
 
     def read_graph(self, vertex_path, edge_path):
         """
@@ -507,13 +520,6 @@ class DFBBsolver:
 
             # Decide whether to backtrack from node
             if self.backtrack(node):
-                ###### DEBUG ########
-                # if (node == 9):
-                #     print("DEBUG")
-                #     print(self.graph.vertex_list[node]['G'])
-                #     print(self.BEST_COSTS)
-                #     z = input()
-                #####################
                 if verbose==1:
                     print("BACKTRACK: ", node)
                     print("BACKTRACKED COST: ", self.graph.vertex_list[node]['F'])
@@ -584,6 +590,306 @@ class DFBBsolver:
             if verbose==1:
                 print("******************************")
 
+class IDMOAsolver:
+
+    def __init__(self, graph, start_node, goal_list):
+
+        # Store graph
+        assert isinstance(graph, Graph)
+        self.graph = graph
+        self.start_node = start_node
+
+        # Keep list of goal nodes
+        self.goal_nodes = set(goal_list)
+
+        # Create Open list initialized with the start node
+        assert start_node in graph.vertex_list
+
+        self.SOLUTIONS = []
+
+        self.reset()
+
+    def reset(self):
+        # Initialize empty sets for closed, open
+        self.CLOSED = set([])
+        self.OPEN = set([self.start_node])
+        self.graph.reset_costs()
+        self.graph.vertex_list[self.start_node]['G'].append((0, 0))
+        # For each G associated with the node, assign an F[i] = G[i] + H
+        self.graph.vertex_list[self.start_node]['F'].append(tuple([sum(x) for x in zip(
+            self.graph.vertex_list[self.start_node]['G'][0],
+            self.graph.vertex_list[self.start_node]['H']
+        )]))
+        self.graph.vertex_list[self.start_node]['path'].append([self.start_node])
+
+    def non_dominated_best_costs(self):
+        """
+        Find set of non-dominated costs in BEST_COSTS
+        """
+        ND = []
+        for item1 in self.SOLUTIONS:
+            nd_flag = True
+            for item2 in self.SOLUTIONS:
+                if item1 != item2:
+                    if self.cost_dominates(item2['G'], item1['G'], objective_idx=1):
+                        # item2 dominates item1
+                        # item1 is not a candidate for ND
+                        nd_flag = False
+                        break
+            if nd_flag is True:
+                # No other item in BEST_COSTS dominates item1
+                # item1 is non-dominated
+                ND.append(item1)
+        return ND
+
+    def backtrack(self, node, threshold, objective_idx):
+        """
+        Decide whether to backtrack from a node based on threshold
+        """
+        assert objective_idx <= 1 and objective_idx >= 0
+        assert isinstance(threshold, int)
+        if objective_idx == 0:
+            assert len(self.graph.vertex_list[node]['G']) == 1
+            cost = self.graph.vertex_list[node]['G'][0][objective_idx]
+            if cost > threshold:
+                return True
+            else:
+                return False
+        else:
+            if len(self.graph.vertex_list[node]['G']) == 1:
+                cost = self.graph.vertex_list[node]['G'][0][objective_idx]
+                if cost > threshold:
+                    return True
+                else:
+                    return False
+            else:
+                cntr = 0
+                remove_G = []
+                for G in self.graph.vertex_list[node]['G']:
+                    cost = G[objective_idx]
+                    if cost > threshold:
+                        remove_G.append(G)
+                        cntr += 1
+                if cntr == len(self.graph.vertex_list[node]['G']):
+                    # All of the paths are worthless
+                    self.graph.vertex_list[node]['G'] = []
+                    self.graph.vertex_list[node]['path'] = []
+                    return True      # So backtrack
+                else:
+                    # Some good paths exist, so don't backtrack but remove bad paths
+                    for G in remove_G:
+                        idx = self.graph.vertex_list[node]['G'].index(G)
+                        self.graph.vertex_list[node]['G'].pop(idx)
+                        self.graph.vertex_list[node]['path'].pop(idx)
+                    return False    # Do not backtrack
+
+    def cost_dominates(self, F1, F2, objective_idx):
+        """
+        Return True if F1 dominates F2 based on objective idx
+        """
+        if objective_idx == 0:
+            return F1[objective_idx] < F2[objective_idx]
+        else:
+            temp = False
+            for elem1, elem2 in zip(F1, F2):
+                if elem2 < elem1:
+                    return False    # all elem1 >= elem2 condition failed
+                elif elem1 < elem2:
+                    temp = True     # at least 1 elem1 > elem2 satisfied
+            return temp
+
+    def search(self, objective_idx, threshold, find_all=False, verbose=1):
+
+        def accrued_non_dominated_paths(n, objective_idx):
+            ND = {'G' : [], 'path' : []}
+            for G1, path1 in zip(self.graph.vertex_list[n]['G'], self.graph.vertex_list[n]['path']):
+                flag = True
+                for G2 in self.graph.vertex_list[n]['G']:
+                    if G1 != G2:
+                        if self.cost_dominates(G2, G1, objective_idx):
+                            # G1 is fully dominated by G2
+                            # So G1 must be removed
+                            flag = False
+                            break
+                if flag is True:
+                    ND['G'].append(G1)
+                    ND['path'].append(path1)
+            return ND
+
+        BACKTRACKED = []
+
+        iter = 1
+        while True:
+
+            if verbose==1:
+                print("Iter: ", iter)
+                print("OPEN: ", self.OPEN)
+                print("CLOSED: ", self.CLOSED)
+                iter += 1
+
+            if len(self.OPEN) == 0:
+                if verbose==1:
+                    print("******************************")
+                # Open list is empty, break
+                break
+
+            node = self.OPEN.pop()
+            self.CLOSED.add(node)
+
+            if self.backtrack(node, threshold, objective_idx):
+                # Node exceeds threshold
+                # Must backtrack
+                if verbose==1:
+                    print("BACKTRACK: ", node)
+                    print("BACKTRACKED COST: ", self.graph.vertex_list[node]['F'])
+                    print("******************************")
+                BACKTRACKED.append({
+                    'node' : node, 
+                    'F' : self.graph.vertex_list[node]['F'],
+                    'path' : self.graph.vertex_list[node]['path']
+                })
+                continue
+
+            # Continue if node is a leaf
+            if node not in self.graph.adjacency_list:
+                if verbose==1:
+                    print("BACKTRACK: ", node)
+                    print("BACKTRACKED COST: ", self.graph.vertex_list[node]['F'])
+                    print("******************************")
+                BACKTRACKED.append({
+                    'node' : node, 
+                    'F' : self.graph.vertex_list[node]['F'],
+                    'path' : self.graph.vertex_list[node]['path']
+                })
+                continue
+
+            if verbose==1:
+                print("EXPAND: ", node)
+                print("******************************")
+
+            for next_node_dict in self.graph.adjacency_list[node]:
+
+                next_node = next_node_dict['next']
+                cost = next_node_dict['cost']
+
+                # Compute G for next node
+                for G in self.graph.vertex_list[node]['G']:
+                    nextG = tuple([sum(x) for x in zip(
+                        G,
+                        cost
+                    )])
+                    assert (len(nextG) == 2)
+                    self.graph.vertex_list[next_node]['G'].append(nextG)
+                # Establish backpointers
+                for path in self.graph.vertex_list[node]['path']:
+                    self.graph.vertex_list[next_node]['path'].append([*path, next_node])
+
+                # Find non-dominated costs
+                nd_costs = accrued_non_dominated_paths(next_node, objective_idx)
+                assert isinstance(nd_costs['G'], list)
+                assert isinstance(nd_costs['path'], list)
+                self.graph.vertex_list[next_node]['G'] = copy.deepcopy(nd_costs['G'])
+                self.graph.vertex_list[next_node]['path'] = copy.deepcopy(nd_costs['path'])
+
+                # Update F for next_node
+                self.graph.vertex_list[next_node]['F'] = []
+                for G in self.graph.vertex_list[next_node]['G']:
+                    self.graph.vertex_list[next_node]['F'].append(tuple([sum(x) for x in zip(
+                        G,
+                        self.graph.vertex_list[next_node]['H']
+                    )]))
+
+                self.OPEN.add(next_node)
+                if next_node in self.CLOSED:
+                    self.CLOSED.remove(next_node)
+
+                # Check if next node is goal node
+                if node in self.goal_nodes and not find_all:
+                    # Goal node found
+                    # Break
+                    BACKTRACKED.append({
+                        'node' : node, 
+                        'F' : self.graph.vertex_list[node]['F'],
+                        'path' : self.graph.vertex_list[node]['path']
+                    })
+                    break
+                elif node in self.goal_nodes:
+                    # Goal node found
+                    # But do not break
+                    BACKTRACKED.append({
+                        'node' : node, 
+                        'F' : self.graph.vertex_list[node]['F'],
+                        'path' : self.graph.vertex_list[node]['path']
+                    })
+
+        return BACKTRACKED
+
+    def IDA_1(self, verbose=0):
+
+        threshold = self.graph.vertex_list[self.start_node]['F'][0][0]
+
+        while True:
+
+            self.reset()
+            assert isinstance(threshold, int)
+            BACKTRACKED = self.search(0, threshold, False, verbose = verbose)
+            assert len(BACKTRACKED) > 0
+
+            min_threshold = 1e10
+            min_idx = -1
+            idx = 0
+            for item in BACKTRACKED:
+                if item['node'] in self.goal_nodes:
+                    return item
+                assert len(item['F']) == 1
+                if item['F'][0][0] < min_threshold:
+                    min_threshold = item['F'][0][0]
+                    min_idx = idx
+                idx += 1
+
+            threshold = min_threshold
+
+    def IDA_2(self, max_threshold, verbose=0):
+
+        SOLUTIONS = []
+
+        self.reset()
+        assert isinstance(max_threshold, int)
+        BACKTRACKED = self.search(1, max_threshold, True, verbose=verbose)
+        assert len(BACKTRACKED) > 0
+
+        for item in BACKTRACKED:
+            if item['node'] in self.goal_nodes:
+                SOLUTIONS.append(item)
+
+        return SOLUTIONS
+
+    def IDMOA(self, verbose=0):
+
+        # Step 1: Perform IDA* on the first objective
+        # to obtain the optimum solution in terms of the same
+        item = self.IDA_1(verbose=verbose)
+
+        # Get the threshold for the second objective
+        new_threshold = item['F'][0][1]
+
+        self.reset()
+        unfiltered_solutions = self.IDA_2(new_threshold, verbose=verbose)
+
+        for item in unfiltered_solutions:
+            for F, path in zip(item['F'], item['path']):
+                self.SOLUTIONS.append({
+                    'node' : item['node'],
+                    'G' : F,
+                    'path' : path
+                })
+
+        self.SOLUTIONS = self.non_dominated_best_costs()
+
+        print("SOLUTIONS FOUND -")
+        for item in self.SOLUTIONS:
+            print("Goal: ", item['node'], "\tCost: ", item['G'], "\tPath: ", item['path'])
+
 def main():
     
     vertex_path = "./input/test_graph_vertex.csv"
@@ -594,10 +900,16 @@ def main():
 
     # moa.MOA(verbose=1)
 
-    G2 = Graph(vertex_path=vertex_path, edge_path=edge_path)
-    dfbb = DFBBsolver(G2, 0, [8, 9, 10])
+    # G2 = Graph(vertex_path=vertex_path, edge_path=edge_path)
+    # dfbb = DFBBsolver(G2, 0, [8, 9, 10])
 
-    dfbb.DFBB(verbose=1)
+    # dfbb.DFBB(verbose=1)
+
+    G3 = Graph(vertex_path=vertex_path, edge_path=edge_path)
+    idmoa = IDMOAsolver(G3, 0, [8, 9, 10])
+
+    idmoa.IDMOA(verbose=1)
+
 
 if __name__ == '__main__':
     main()
